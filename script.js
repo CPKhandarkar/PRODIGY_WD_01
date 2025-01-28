77cf7d2ea536de92b019d1ba4277439282edbe00
Chalbe <132476165+CPKhandarkar@users.noreply.github.com>
2025-01-28 13:49:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+document.querySelectorAll(".nav-links a").forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
